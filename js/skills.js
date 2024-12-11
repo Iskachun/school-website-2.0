5853fc7e3bb1dee92151d392e1106ee039faf646
iskachun <iskachun@proton.me>
2024-12-11 14:53:05 +0100

    asasa

---
+++ js/skills.js
@@ -0,0 +1,16 @@
+var  abox = document.getElementsByClassName("html")[0];
+var  abox3 = document.getElementsByClassName("js")[0];
+var  abox2 = document.getElementsByClassName("css")[0];
+function allmove(){
+       abox.classList.toggle("move");
+       abox2.classList.toggle("move2");
+       abox3.classList.toggle("move3");
+}
+
+
+var  abox4 = document.getElementsByClassName("cpp")[0];
+var  abox5 = document.getElementsByClassName("python")[0];
+function allmove2(){
+       abox4.classList.toggle("move4");
+       abox5.classList.toggle("move5");
+}
